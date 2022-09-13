462570e34a00b474d414a42372081076d8d076e1
Пиксель <Zelenskiy2013@gmail.com>
2022-09-13 20:14:51 +0300

    Update file

---
+++ Homework lesson #6 Зеленский Андрей.py
@@ -0,0 +1,23 @@
+## TASK №1 ######################################################
+
+### Version 1 ############
+my_list = [45, 30, 58, 120, 78, 168, -40, -160, 0, 3]
+for number in my_list:
+    if number > 100:
+        print(number)
+
+## TASK №2 ######################################################
+my_list = [45, 30, 58, 120, 78, 168, -40, -160, 0, 3]
+my_results = []
+for number in my_list:
+    if number > 100:
+        my_results.append(number)
+print(my_results)
+
+## TASK №3 ######################################################
+
+my_list = [45, 30, 58, 120, 78, 168, -40, -160, 0, 3]
+if len(my_list) < 2:
+    my_list.append(0)
+elif len(my_list) >= 2:
+    my_list.append(my_list[-1] + my_list[-2])
---
+++ lesson_1.py
@@ -0,0 +1,5 @@
+my_list = list("qwerty")
+
+my_str = "".join(my_list[::-1])
+
+print(my_str[::-1])
