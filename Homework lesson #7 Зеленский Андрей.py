e4ae857ac2346644547d3bb68ce061e7a111a99f
Пиксель <Zelenskiy2013@gmail.com>
2022-09-19 18:15:16 +0300

    Update file

--- Homework lesson #7 Зеленский Андрей.py
+++ Homework lesson #7 Зеленский Андрей.py
@@ -91,11 +91,11 @@ my_str = "ERggggggggJrtyujhgfwerT$%^#$%&Y"
 
         ## Вариант решения №1 ##(без учета больших букв)
 
-my_new_list_1 = list(set(my_str))
+my_new_list_1 = [element for element in my_str if my_str.count(element) == 1]
 
         ## Вариант решения №2 ##(с учетом больших букв)
 
-my_new_list_2 = list(set(my_str.lower()))
+my_new_list_2 = [element for element in my_str if my_str.lower().count(element) == 1]
 
 # TASK №12######################################################
 
