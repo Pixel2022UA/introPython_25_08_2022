939f391659cb4f1fd715fb63e0d4f2e0080328d1
Пиксель <Zelenskiy2013@gmail.com>
2022-10-03 16:40:10 +0300

    Update file

--- Homework lesson #9 Зеленский Андрей.py
+++ Homework lesson #9 Зеленский Андрей.py
@@ -1,11 +1,11 @@
 import random as rnd
-import string as str
-
+import string as str_
 
 my_list = ["dsfsgh", "atyqwet", "1", "2qwer", "34yahg", "fdhhh", "wfeh", "qwe"]
-my_list_2 = ["asfsgh", "tyqwet", 1 , "2qAwer", 45, 78, "afeh", "qwe"]
+my_list_2 = ["asfsgh", "tyqwet", 1, "2qAwer", 45, 78, "afeh", "qwe"]
 my_str = "WE*RHz2356ugsgh34567seRh"
 my_str_2 = "zqabcfvVti*IgveefgjkR#"
+
 
 ### TASK №1 ######################################################
 
@@ -19,6 +19,9 @@ def sort_list(my_list):
     return my_new_list
 
 
+sort_list(my_list)
+
+
 ### TASK №2 ######################################################
 
 def list_with_a_first(my_list):
@@ -27,6 +30,10 @@ def list_with_a_first(my_list):
         if "a" == element[0]:
             my_new_list_1.append(element)
     return my_new_list_1
+
+
+list_with_a_first(my_list)
+
 
 ### TASK №3 ######################################################
 
@@ -37,6 +44,10 @@ def list_with_a(my_list):
             my_new_list_2.append(element)
     return my_new_list_2
 
+
+list_with_a(my_list)
+
+
 ### TASK №4 ######################################################
 
 def list_str(my_list_2):
@@ -45,7 +56,11 @@ def list_str(my_list_2):
         if element is str(element):
             my_new_list_3.append(element)
     return my_new_list_3
-print(list_str(my_list_2))
+
+
+list_str(my_list_2)
+
+
 ### TASK №5 ######################################################
 
 def onetime_elem_in_str(my_str):
@@ -55,11 +70,17 @@ def onetime_elem_in_str(my_str):
             my_new_list_4.append(elem)
     return my_new_list_4
 
+
+onetime_elem_in_str(my_str)
+
 ### TASK №6 ######################################################
 
 def inter_elem(my_str, my_str_2):
     my_new_list_5 = list((set(my_str).intersection(set(my_str_2))))
     return my_new_list_5
+
+
+inter_elem(my_str, my_str_2)
 
 ### TASK №7 ######################################################
 
@@ -70,17 +91,25 @@ def onetime_inter_elem(my_str, my_str_2):
             my_new_list_6.append(element)
     return my_new_list_6
 
+
+onetime_inter_elem(my_str, my_str_2)
+
 ### TASK №8 ######################################################
 
 names = ["Jack", "Piter", "Liza", "Elvis"]
 domains = ["com", "ua", "com.ua", "biz"]
 
+
 def email_generator(names, domains):
     rnd_name = rnd.choice(names)
     rnd_domain = rnd.choice(domains)
     rnd_int_to_str = rnd.randint(100, 999)
-    rnd_str = "".join(rnd.choice(str.ascii_lowercase) for i in range(rnd.randint(5, 7)))
+    rnd_str = "".join(rnd.choice(str_.ascii_lowercase) for i in range(rnd.randint(5, 7)))
     rnd_email = "".join(f"{rnd_name}.{rnd_int_to_str}@{rnd_str}.{rnd_domain}")
     return rnd_email
     # или сразу return f"{rnd_name}.{rnd_int_to_str}@{rnd_str}.{rnd_domain}")
 
+
+email_generator(names, domains)
+
+###################################################################
