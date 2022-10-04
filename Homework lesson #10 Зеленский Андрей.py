81e1924f0280c6201af9382f7d8a23b1e1a581c8
Пиксель <Zelenskiy2013@gmail.com>
2022-10-04 12:41:14 +0300

    Update file

--- Homework lesson #10 Зеленский Андрей.py
+++ Homework lesson #10 Зеленский Андрей.py
@@ -1,18 +1,22 @@
-
 import re
-
 
 ### TASK №1 ######################################################
 
 filename_1 = "domains.txt"
+
+
 def domains_list(filename_1):
     with open(filename_1, "r") as file:
         data = [domains.strip().replace(".", "") for domains in file.readlines()]
     return data
 
+
+domains_list(filename_1)
+
 ### TASK №2 ######################################################
 
 filename_2 = "names.txt"
+
 
 def second_names_list(filename_2):
     names_list = []
@@ -21,20 +25,51 @@ def second_names_list(filename_2):
             names_list.append(elem.rsplit("\t")[1])
     return names_list
 
+
+second_names_list(filename_2)
+
 ### TASK №3 ######################################################
 
 filename_3 = "authors.txt"
 
+
 def sort_def(filename_3):
     dict_dates = []
-    pattern = r'\d{1,2}\w{2}\s\w{3,10}\s\d{4}'
     with open(filename_3, "r") as file_txt:
-        str_ = [line.strip() for line in file_txt.readlines()]
-        for elem in str_:
-            dates = "".join(re.findall(pattern, elem))
-            if len(dates):
-                dict_dates.append({"date": dates})
+        dates_list = [line.strip().split(" - ")[0] for line in file_txt.readlines() if "-" in line]
+        for elem in dates_list:
+            if len(elem.split()) == 3:
+                dict_dates.append([{"date": elem}])
     return dict_dates
 
 
+sort_def(filename_3)
+
 ### TASK №4 ######################################################
+
+import calendar
+
+filename_3 = "authors.txt"
+
+def sort_def(filename_3):
+    dates_2 = []
+    month_dict = {name: num for num, name in enumerate(calendar.month_name) if num}
+    my_dict_keys = ["date_original", "date_modified"]
+    with open(filename_3, "r") as file_txt:
+        date_1 = [line.strip().split(" - ")[0] for line in file_txt.readlines() if "-" in line]
+        for elem in date_1:
+            if len(elem.split()) == 3:
+                dd, mm, yyyy = elem.split()
+                dd = int(dd[:-2])
+                for key in month_dict.keys():
+                    if key == mm:
+                        mm = month_dict[key]
+            dates_2.append(".".join(['{:02}'.format(dd), '{:02}'.format(mm), yyyy]))
+    my_dict_values = zip(date_1, dates_2)
+    my_dict = [dict(zip(my_dict_keys, values)) for values in my_dict_values]
+    return my_dict
+
+
+sort_def(filename_3)
+
+####################################################################
--- lesson_1.py
+++ lesson_1.py
@@ -1,54 +1,24 @@
-# import calendar
-#
-# filename_3 = "authors.txt"
-#
-# def sort_def(filename_3):
-#     dates_1 = []
-#     dates_2 = []
-#     month_dict = {name: num for num, name in enumerate(calendar.month_name) if num}
-#     with open(filename_3, "r") as file_txt:
-#         str_ = [line.strip().split(" - ") for line in file_txt.readlines() if len(line) > 10]
-#         for elem in str_:
-#             dates_1.append(elem[0])
-#         for elem in dates_1:
-#             dd, mm, yyyy = elem.split()
-#             dd = int(dd[:-2])
-#             for key in month_dict.keys():
-#                 if key == mm:
-#                     mm = month_dict[key]
-#             dates_2.append(".".join(['{:02}'.format(dd), '{:02}'.format(mm), yyyy]))
-#         my_dict_values = zip(dates_1, dates_2)
-#         my_dict_keys = ["date_original", "date_modified"]
-#         my_dict = [dict(zip(my_dict_keys, values)) for values in my_dict_values]
-#     return my_dict
-#
-# print(sort_def(filename_3))
+import calendar
 
-#
-# import calendar
-#
-# filename_3 = "authors.txt"
-#
-# def sort_def(filename_3):
-#     dates_1 = []
-#     dates_2 = []
-#     month_dict = {name: num for num, name in enumerate(calendar.month_name) if num}
-#     with open(filename_3, "r") as file_txt:
-#         str_ = [line.strip().split(" - ") for line in file_txt.readlines() if len(line) > 10]
-#         for elem in str_:
-#             dates_1.append(elem[0])
-#         for elem in dates_1:
-#             dd, mm, yyyy = elem.split()
-#             dd = int(dd[:-2])
-#             for key in month_dict.keys():
-#                 if key == mm:
-#                     mm = month_dict[key]
-#             dates_2.append(".".join(['{:02}'.format(dd), '{:02}'.format(mm), yyyy]))
-#         my_dict_values = zip(dates_1, dates_2)
-#         my_dict_keys = ["date_original", "date_modified"]
-#         my_dict = [dict(zip(my_dict_keys, values)) for values in my_dict_values]  # еще можно добавить проверку if len(dates_1) == len(dates_2)
-#     return my_dict
-#
-# print(sort_def(filename_3))
+filename_3 = "authors.txt"
 
+def sort_def(filename_3):
+    dates_2 = []
+    month_dict = {name: num for num, name in enumerate(calendar.month_name) if num}
+    my_dict_keys = ["date_original", "date_modified"]
+    with open(filename_3, "r") as file_txt:
+        date_1 = [line.strip().split(" - ")[0] for line in file_txt.readlines() if "-" in line]
+        for elem in date_1:
+            if len(elem.split()) == 3:
+                dd, mm, yyyy = elem.split()
+                dd = int(dd[:-2])
+                for key in month_dict.keys():
+                    if key == mm:
+                        mm = month_dict[key]
+            dates_2.append(".".join(['{:02}'.format(dd), '{:02}'.format(mm), yyyy]))
+    my_dict_values = zip(date_1, dates_2)
+    my_dict = [dict(zip(my_dict_keys, values)) for values in my_dict_values]
+    return my_dict
+
+print(sort_def(filename_3))
 
