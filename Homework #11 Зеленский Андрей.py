8f35e84eae5c08087e596ae8d187fa70096bed0d
Пиксель <Zelenskiy2013@gmail.com>
2022-10-05 16:50:19 +0300

    Update file

---
+++ Homework #11 Зеленский Андрей.py
@@ -0,0 +1,54 @@
+import random as rnd
+import string as str_
+import json
+import os
+
+
+### TASK №1 ######################################################
+
+def generate_txt_data():
+    symbols = str_.printable[0:62] + " "
+    rnd_str = "".join(rnd.choice(symbols) for i in range(rnd.randint(100, 1000)))
+    return rnd_str
+
+
+generate_txt_data()
+
+
+### TASK №2 ######################################################
+
+def generate_json_data():
+    def rnd_keys():
+        key = "".join(rnd.choice(str_.ascii_lowercase) for i in range(5))
+        return key
+
+    def rnd_vals():
+        val_1 = rnd.uniform(0, 1)
+        val_2 = rnd.randint(-100, 100)
+        val_3 = rnd.choice([True, False])
+        list_vols = rnd.choice([val_1, val_2, val_3])
+        return list_vols
+
+    rnd_dict = {rnd_keys(): rnd_vals() for elem in range(rnd.randint(5, 20))}
+    return rnd_dict
+
+
+generate_json_data()
+
+### TASK №3 ######################################################
+
+dirname = "C:/Users/Пиксель/PycharmProjects/introPython_25_08_2022/random.json"
+
+def generate_and_write_file(dirname: str):
+    if os.path.splitext(dirname)[1][1:] == "json":
+        with open(dirname, 'w') as json_file:
+            json_file.write(json.dumps(generate_json_data()))
+    elif os.path.splitext(dirname)[1][1:] == "txt":
+        with open(dirname, "w") as file:
+            file.write(generate_txt_data())
+    else:
+        print("Unsupported file format")
+    return
+
+
+generate_and_write_file(dirname)
--- lesson_1.py
+++ lesson_1.py
@@ -1,24 +1,36 @@
-import calendar
-
-filename_3 = "authors.txt"
-
-def sort_def(filename_3):
-    dates_2 = []
-    month_dict = {name: num for num, name in enumerate(calendar.month_name) if num}
-    my_dict_keys = ["date_original", "date_modified"]
-    with open(filename_3, "r") as file_txt:
-        date_1 = [line.strip().split(" - ")[0] for line in file_txt.readlines() if "-" in line]
-        for elem in date_1:
-            if len(elem.split()) == 3:
-                dd, mm, yyyy = elem.split()
-                dd = int(dd[:-2])
-                for key in month_dict.keys():
-                    if key == mm:
-                        mm = month_dict[key]
-            dates_2.append(".".join(['{:02}'.format(dd), '{:02}'.format(mm), yyyy]))
-    my_dict_values = zip(date_1, dates_2)
-    my_dict = [dict(zip(my_dict_keys, values)) for values in my_dict_values]
-    return my_dict
-
-print(sort_def(filename_3))
-
+# import calendar
+#
+# filename_3 = "authors.txt"
+#
+# def sort_def(filename_3):
+#     dates_2 = []
+#     month_dict = {name: num for num, name in enumerate(calendar.month_name) if num}
+#     my_dict_keys = ["date_original", "date_modified"]
+#     with open(filename_3, "r") as file_txt:
+#         date_1 = [line.strip().split(" - ")[0] for line in file_txt.readlines() if "-" in line]
+#         for elem in date_1:
+#             if len(elem.split()) == 3:
+#                 dd, mm, yyyy = elem.split()
+#                 dd = int(dd[:-2])
+#                 for key in month_dict.keys():
+#                     if key == mm:
+#                         mm = month_dict[key]
+#             dates_2.append(".".join(['{:02}'.format(dd), '{:02}'.format(mm), yyyy]))
+#     my_dict_values = zip(date_1, dates_2)
+#     my_dict = [dict(zip(my_dict_keys, values)) for values in my_dict_values]
+#     return my_dict
+#
+# print(sort_def(filename_3))
+#
+#
+#
+#
+#
+# key = "".join(rnd.choice(str_.ascii_lowercase) for i in range(5))
+# val_1 = rnd.uniform(0, 1)
+# val_2 = rnd.randint(-100, 100)
+# val_3 = rnd.choice([True, False])
+# list_option = rnd.choice([val_1, val_1, val_3])
+#
+#
+# print(val_1, val_1, val_3)
